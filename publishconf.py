ee6ee788d70d4afc95e3e59fd45c0f1fd510d9fd
Mike Boyle <mboyle1357@gmail.com>
2025-10-17 07:23:13 +0100

    Set SITEURL to Netlify domain and disable RELATIVE_URLS

--- publishconf.py
+++ publishconf.py
@@ -1,6 +1,10 @@
+import os, sys
+sys.path.insert(0, os.path.dirname(__file__))  # ensure we can import pelicanconf
 from pelicanconf import *
 
-SITEURL = "https://example.com"  # <-- change to your domain
+
+
+SITEURL = "michaelboyle.netlify.app"  # <-- change to your domain
 RELATIVE_URLS = False
 FEED_ALL_RSS = "rss.xml"
 DELETE_OUTPUT_DIRECTORY = True
